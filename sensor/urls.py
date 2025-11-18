8e7ac7e17da14700f890ae36f723cb5ae4ed6179
RJ Conanan <rauljarodconanan@gmail.com>
2025-11-18 17:54:06 +0800

    added live reading and alert view urls

--- sensor/urls.py
+++ sensor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ingest_reading, LiveSensorViewSet, SensorListView, SensorDetailView
+from .views import (ingest_reading, LiveSensorViewSet, SensorListView,
+                    SensorDetailView, sensor_detail_alert_view, sensor_detail_reading_view)
 
 router = DefaultRouter()
 router.register("live-sensors", LiveSensorViewSet, basename="live-sensors")
@@ -8,6 +9,8 @@ router.register("live-sensors", LiveSensorViewSet, basename="live-sensors")
 urlpatterns = [
     path('list/', SensorListView.as_view(), name='sensor-list'),
     path('detail/<str:id>/', SensorDetailView.as_view(), name='sensor-detail'),
+    path('detail/<str:id>/readings/', sensor_detail_reading_view, name='sensor-detail-readings'),
+    path('detail/<str:id>/alerts/', sensor_detail_alert_view, name='sensor-detail-alerts'),
     path('ingest/', ingest_reading, name='sensor-ingest'),
     path("", include(router.urls)),
     ]
